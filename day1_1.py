3e885eb44075c3d5f0af0d2dbf4e171d66704430
hukui <1554937835@qq.com>
2019-10-08 19:17:45 +0800

    kkkkkkk

--- day1_1.py
+++ day1_1.py
@@ -1,5 +1,4 @@
-# GET请求
-from urllib import request as ur
+
 
 url = 'https://fe-api.zhaopin.com/c/i/sou?pageSize=90&cityId=530&workExperience=-1&education=-1&companyType=-1&employmentType=-1&jobWelfareTag=-1&kw=python&kt=3&_v=0.38498314&x-zp-page-request-id=4aea81ef10fa4b6aabf8319073c0ebf0-1567999305242-790668&x-zp-client-id=55724e1f-bf4a-41b6-895d-26f229442f8d'
 
